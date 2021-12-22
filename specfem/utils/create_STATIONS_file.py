73ee69d638ee2c5ba0e25f974e208f640166467f
pedro-mrtnz <74901477+pedro-mrtnz@users.noreply.github.com>
2021-12-22 13:00:46 +0100

    Text format subtleties

--- specfem/utils/create_STATIONS_file.py
+++ specfem/utils/create_STATIONS_file.py
@@ -42,7 +42,7 @@ def create_stations_file(rec_lims, pos_ini, nsts_x, nsts_z, dest_dir='./DATA'):
             network = "AA"                    # Network station
             
             f.write(
-                f"{st_name:<10s}\t{network:<10s}\t{f'{x:.2f}':<15s}\t{f'{z:.2f}':<15s}\t{f'{0.0}':<15s}\t{f'{0.0}':<15s}\n")
+                f"{st_name:<10s}\t{network:<10s}\t{f'{x:.2f}':<15s}\t{f'{z:.2f}':<15s}\t{str(0.0):<15s}\t{str(0.0):<15s}\n")
     
     f.close()
                         
