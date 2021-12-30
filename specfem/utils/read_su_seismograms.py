e8414311cdd8bd27d8b563b65b35273e70cdc1b0
pedro-mrtnz <74901477+pedro-mrtnz@users.noreply.github.com>
2021-12-30 19:14:33 +0100

    ylabel and xlabel exchanged

--- specfem/utils/read_su_seismograms.py
+++ specfem/utils/read_su_seismograms.py
@@ -64,8 +64,8 @@ def plot_seis(time, data, vmin_max=False, title='', interpolation=''):
                         extent = extent,
                         title = title,
                         interpolation = interpolation)
-    plt.xlabel('Receiver number')
-    plt.ylabel('Time [s]')
+    plt.ylabel('Receiver number')
+    plt.xlabel('Time [s]')
 
 
 if __name__ == "__main__":
