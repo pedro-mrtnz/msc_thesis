7dfaee2caeecb4090d7a73e6d5e22e19574df956
pedro-mrtnz <74901477+pedro-mrtnz@users.noreply.github.com>
2022-01-12 11:41:19 +0100

    Plotting functions added

--- msc/specfem/utils/nmo_correction.py
+++ msc/specfem/utils/nmo_correction.py
@@ -1,5 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from pandas.core.base import DataError
+import tqdm as tqdm
 from scipy import interpolate
+
+class ConfigPlots:
+    aspect        = 'auto'
+    cmap          = 'Greys'
+    interpolation = 'none'
+    fig_size      = (8,8)
+
 
 def reflection_time(t0, x, vnmo):
     """
@@ -63,7 +73,7 @@ def nmo_correction(cmp, dt, offsets, velocities):
     nmo = np.zeros_like(cmp)
     n_samples = cmp.shape[0]
     times = np.arange(0, n_samples*dt, dt)
-    for i, t0 in enumerate(times):
+    for i, t0 in tqdm(enumerate(times)):
         for j, x in enumerate(offsets):
             t = reflection_time(t0, x, velocities[i])
             A = sample_trace(cmp[:, j], t, dt)          # Amplitude
@@ -72,3 +82,51 @@ def nmo_correction(cmp, dt, offsets, velocities):
                 nmo[i, j] = A
 
     return nmo
+
+def plot_cmp(cmp_data, time, t0, x_offsets, vnmo):
+    """
+    Plot CMP data (not-corrected) with instance of reflected times for a given t0.
+
+    Args:
+        cmp_data  (2D array): CMP data 
+        time      (1D array): Two Way Time 
+        t0        (float)   : time instance upon which we compute the reflected times
+        x_offsets (1D array): offsets positions
+        vnmo      (float)   : normal velocity
+    """
+    refl_times = reflection_time(t0=t0, x=x_offsets, vnmo=vnmo)
+    
+    fig = plt.figure(figsize=ConfigPlots.fig_size)
+    ax = fig.add_subplot(111)
+    vmin_max = np.percentile(cmp_data, 99.)
+    extent = [1, cmp_data.shape[1], time[-1], time[0]]
+    ax.imshow(
+        cmp_data,
+        aspect = ConfigPlots.aspect,
+        cmap = ConfigPlots.cmap,
+        vmin = -vmin_max,
+        vmax = vmin_max,
+        interpolation = ConfigPlots.interpolation,
+        exten = extent
+    )
+    ax.plot(np.arange(1, len(x_offsets)+1), refl_times, '-r', lw=3)
+    ax.set(xlabel='Receiver number', ylabel='TWT [s]')
+    
+def plot_nmo(nmo_data, time):
+    """ 
+    Plot the NMO corrected data.
+    """
+    fig = plt.figure(figsize=ConfigPlots.fig_size)
+    ax = fig.add_subplot(111)
+    vmin_max = np.percentile(nmo_data, 99.)
+    extent = [1, nmo_data.shape[1], time[-1], time[0]]
+    ax.imshow(
+        nmo_data,
+        aspect = ConfigPlots.aspect,
+        cmap = ConfigPlots.cmap,
+        vmin = -vmin_max,
+        vmax = vmin_max,
+        interpolation = ConfigPlots.interpolation,
+        exten = extent
+    )
+    ax.set(xlabel='Receiver number', ylabel='TWT [s]')
